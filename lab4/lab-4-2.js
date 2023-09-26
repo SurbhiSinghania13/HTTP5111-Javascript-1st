7062d3e5d6fda492de4e40f511b5d2b5aad50ba4
Bob <surbhisinghania404@gmail.com>
2023-09-26 13:09:54 -0400

    lab4 added

---
+++ lab4/lab-4-2.js
@@ -0,0 +1,26 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 2:  AN AVERAGE FUNCTION
+var htt5110=95;
+var http5111=86;
+var http5112=70;
+var http5114=90;
+var ixd5106=80;
+
+//################## CREATE YOUR AVERAGE FUNCTION
+//This function takes five numbers and returns their average to one decimal place.
+function averageNumfun(num1,num2,num3,num4,num5){
+    var totalAverage= (num1+num2+num3+num4+num5)/5;
+    return totalAverage.toFixed(1);
+}
+
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+if(averageNumfun(htt5110,http5111,http5112,http5114,ixd5106)>=70)
+{
+    alert("You passed the course");
+}
+else{
+    alert("Review required");
+}
+//var output= averageNumfun(5,10,15,20,25);
+//console.log(output);
---
+++ lab4/lab-4-3.js
@@ -0,0 +1,28 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 3:  WALKING THE DOG 
+
+
+//################## CREATE YOUR checkTemp FUNCTION
+//This function will check the current temperature and return true or false
+function checkTemp(currentTemp){
+    if(currentTemp>30 || currentTemp<-10){
+        return false;
+    }
+    else{
+        return true;
+    }
+
+}
+//It expects to receive a temperature value in the prompt box
+//It will return false if user input temperature greater than 30 or less than -10, otherwise true
+
+
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+var askcurrentTemp = prompt("What is the current Temperature");
+if(checkTemp(askcurrentTemp)==false){
+    alert("Yikes!Too hot/cold for dog walking")
+}
+else{
+    alert("You're good,have a nice walk");
+}
